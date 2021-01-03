35e55e2885defabcd28f465c45f62525b56b4455
singhst <singhst@users.noreply.github.com>
2021-01-03 19:24:39 +0800

    can executing custom SQL directly in models.py
    Update the cell value in `Country` column through PUT request where `id=2701` by the `fun_sql_cursor_update()` func in `models.py`.
    
    P.S.
    In `views.py`, the code changes to `@action(methods=['put'], detail=True)`, and the `detail` must be `True`.
    
    The request, `PUT`, http://127.0.0.1:8000/api_v2/study/2701/sql_cursor_update/

--- sqlite-drf/study_info/study_info/study/models.py
+++ sqlite-drf/study_info/study_info/study/models.py
@@ -14,7 +14,7 @@ class Study(models.Model):
     class Meta:
         db_table = "study"
 
-# === By Manager.raw(), raw SQL query ==============
+# === (1) By Manager.raw(), raw SQL query ==============
 def fun_raw_sql_query(**kwargs):
     fee = kwargs.get('fee')
     if fee:
@@ -22,3 +22,47 @@ def fun_raw_sql_query(**kwargs):
     else:
         result = Study.objects.raw('SELECT * FROM study')
     return result
+# === (1) By Manager.raw(), raw SQL query ==============
+
+
+# === (2) Executing custom SQL directly ==============
+from collections import namedtuple
+
+from django.db import connection
+
+def namedtuplefetchall(cursor):
+    # Return all rows from a cursor as a namedtuple
+    desc = cursor.description
+    nt_result = namedtuple('Result', [col[0] for col in desc])
+    return [nt_result(*row) for row in cursor.fetchall()]
+
+#簡單說明一下這段 code，前端可以帶入 id 和 song 來更新資料
+def fun_sql_cursor_update(**kwargs):
+    country = kwargs.get('country')
+    pk = kwargs.get('pk')
+
+    '''
+    Note that if you want to include literal percent signs in the query,
+    you have to double them in the case you are passing parameters:
+    '''
+    with connection.cursor() as cursor:
+        cursor.execute("UPDATE study SET Country = %s WHERE id = %s", [country, pk])
+        cursor.execute("SELECT * FROM study WHERE id = %s", [pk])
+        # result = cursor.fetchone()
+        result = namedtuplefetchall(cursor)
+    result = [
+        {
+            'id': r.id,
+            'Programme': r.Programme,
+            'University': r.University,
+            'Country': r.Country,
+            'Address': r.Address,
+            'Latitude': r.Latitude,
+            'Longitude': r.Longitude,
+            'Programme_Fee': r.Programme_Fee,
+        }
+        for r in result
+    ]
+
+    return result
+# === (2) Executing custom SQL directly ==============
--- sqlite-drf/study_info/study_info/study/views.py
+++ sqlite-drf/study_info/study_info/study/views.py
@@ -11,22 +11,38 @@ from rest_framework import viewsets
 #     queryset = Study.objects.all()
 #     serializer_class = StudySerializer
 
-# === By Manager.raw(), raw SQL query ==============
+# === (1) By Manager.raw(), raw SQL query ==============
 from rest_framework.decorators import action
 from study.models import fun_raw_sql_query
 from rest_framework.response import Response
 from rest_framework import viewsets, status
+# === (1) By Manager.raw(), raw SQL query ==============
+
+# === (2) Executing custom SQL directly ==============
+from study.models import fun_sql_cursor_update
+# === (2) Executing custom SQL directly ==============
 
 # Create your views here.
 class StudyViewSet(viewsets.ModelViewSet):
     queryset = Study.objects.all()
     serializer_class = StudySerializer
 
-    # /api_v2/study/raw_sql_query/
+    # === (1) By Manager.raw(), raw SQL query ==============
+    # /api_v2/study/raw_sql_query/?fee=50000
     @action(methods=['get'], detail=False)
     def raw_sql_query(self, request):
         fee = request.query_params.get('fee', None)
         study = fun_raw_sql_query(fee=fee)
         serializer = StudySerializer(study, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-# === By Manager.raw(), raw SQL query ==============
+    # === (1) By Manager.raw(), raw SQL query ==============
+
+    # === (2) Executing custom SQL directly ==============
+    # /api_v2/study/{pk}/sql_cursor_update/
+    @action(methods=['put'], detail=True)
+    def sql_cursor_update(self, request, pk=None):
+        country = request.data.get('Country', None)
+        if country:
+            study_info = fun_sql_cursor_update(country=country, pk=pk)
+            return Response(study_info, status=status.HTTP_200_OK)
+    # === (2) Executing custom SQL directly ==============
